c0bb81b3cc56c1f8b8104ec5aa7074400e6daa8c
chenjb <anotherchase@gmail.com>
2019-09-20 17:05:28 +0800

    feat(stylelint): support stylelint

---
+++ .stylelintrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+  extends: ['stylelint-config-standard', 'stylelint-config-prettier'],
+  plugins: ['stylelint-scss', 'stylelint-prettier'],
+  rules: {
+    'prettier/prettier': true
+  }
+};
