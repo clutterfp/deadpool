f6838c9245af38834ad38e76abd2908e2738618f
chenjb <anotherchase@gmail.com>
2019-09-21 21:14:36 +0800

    build(npm): fix dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,12 @@
 'use strict';
 
+const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 // style files regexes
+const cssRegex = /\.css$/;
+const cssModuleRegex = /\.module\.css$/;
 const sassRegex = /\.(scss|sass)$/;
 const sassModuleRegex = /\.module\.(scss|sass)$/;
 
@@ -12,6 +15,15 @@ module.exports = function(webpackEnv) {
   const isEnvProduction = webpackEnv === 'production';
 
   return {
-    mode: isEnvProduction ? 'production' : isEnvDevelopment && 'development'
+    mode: isEnvProduction ? 'production' : isEnvDevelopment && 'development',
+    entry: './src/index.tsx',
+    output: {
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+      rules: []
+    },
+    plugins: []
   };
 };
